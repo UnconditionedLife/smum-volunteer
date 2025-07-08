7283e31feead098aec2fcbb3eae7fcffac91044a
Jose Leal <jleal67@gmail.com>
2025-07-08 10:02:13 -0700

    Update GetVolunteerByIdFunction.js

--- src/utils/lambdas/GetVolunteerByIdFunction.js
+++ src/utils/lambdas/GetVolunteerByIdFunction.js
@@ -3,14 +3,21 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
 
 const client = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,GET"
+};
 
 export const handler = async (event) => {
   try {
     const id = event.pathParameters?.id;
 
+
     if (!id) {
       return {
         statusCode: 400,
+        headers: corsHeaders,
         body: JSON.stringify({ message: "Missing volunteer ID in path" }),
       };
     }
@@ -23,19 +30,22 @@ export const handler = async (event) => {
     if (!result.Item) {
       return {
         statusCode: 404,
+        headers: corsHeaders,
         body: JSON.stringify({ message: "Volunteer not found" }),
       };
     }
 
     return {
       statusCode: 200,
+      headers: corsHeaders,
       body: JSON.stringify(result.Item),
     };
   } catch (err) {
     console.error("Error getting volunteer:", err);
     return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Failed to get volunteer" }),
+        statusCode: 500,
+        headers: corsHeaders,
+        body: JSON.stringify({ message: "Failed to get volunteer" }),
     };
   }
 };
